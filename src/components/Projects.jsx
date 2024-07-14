c37d17fc8d4aceaadb198abae7fd3212f4edb910
Char Z <2003charz@gmail.com>
2024-07-13 20:45:48 -0700

    feat: :art: boxes for text
    styling focusing on music and contact sections with making boxes for body text

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,9 +4,9 @@ export default function Projects () {
     return (
         <div id='projects' 
         className='flex justify-center py-24 bg-slate-50 space-x-10'>
-            <div className='flex flex-col space-y-3 text-slate-800 w-3/6'>
+            <div className='flex flex-col space-y-3 w-3/6'>
 
-                <div className='flex flex-row justify-start items-center space-x-3'>
+                <div className='flex flex-row justify-start items-center text-slate-800 space-x-3'>
                     <div className='h-10 bg-slate-800 w-2'></div> 
                     <div className='text-3xl font-semibold'>projects</div>  
                 </div>
